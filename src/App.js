e2d8253f2ef98b95e8ceb16b5946a82454d21d65
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2023-04-01 13:50:56 -0400

    sign in page changes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./config/theme.config";
+
 import SignIn from "./containers/SignIn.jsx"
 
 
 function App() {
   return (
     <Router>
+      <ThemeProvider theme={theme}/>
       <Switch>
         <Route exact path="/">
           <SignIn/>
         </Route>
         <Route path="/sign-up">Signup</Route>
       </Switch>
+      <ThemeProvider/>
     </Router>
   );
 }
