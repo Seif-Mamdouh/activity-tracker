9521097255b8730e3bd8b8e9bf660af93d7166fa
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2023-04-01 13:12:04 -0400

    signin page and env setuo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import SignIn from "./containers/SignIn.jsx"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <SignIn/>
+        </Route>
+        <Route path="/sign-up">Signup</Route>
+      </Switch>
+    </Router>
   );
 }
 
